b689889f63cf9d53b16d84646eee10b6433d2349
Brad <55059632+Azio0@users.noreply.github.com>
2025-03-21 23:09:54 +0000

    Create PixelStorageOne class

--- source/app.py
+++ source/app.py
@@ -5,6 +5,57 @@ from utils.pillow.worker import *
 from utils.transaction.worker import *
 from utils.ziparchive.worker import *
 
+class PixelStorageOne:
+    class deconstruct:
+        def Image(image_path):
+            try:
+                response, status = DeconstructImage(image_path)
+
+                if status != 200:
+                    raise Exception(f'Error {response}')
+                
+                return response, status
+            
+            except Exception as error:
+                return error, 500
+
+        def ZipFile(zip_path):
+            try:
+                response, status = DeconstructZipFile(zip_path)
+
+                if status != 200:
+                    raise Exception(f'Error {response}')
+
+                return response, status
+            
+            except Exception as error:
+                return error, 500
+        
+    class retrieve:
+        def Image(receiptUID):
+            try:
+                response, status = RetrieveImage(receiptUID)
+
+                if status != 200:
+                    raise Exception(f'Error {response}')
+                
+                return response, status
+        
+            except Exception as error:
+                return error, 500
+            
+        def ZipFile(receiptUID):
+            try:
+                response, status = RetrieveZipFile(receiptUID)
+
+                if status != 200:
+                    raise Exception(f'Error {response}')
+                
+                return response, status
+            
+            except Exception as error:
+                return error, 500
+
 if __name__ == "__main__":
     create_image_table()
     create_zip_table()
